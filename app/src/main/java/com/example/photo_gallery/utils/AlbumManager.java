a2ddcdfd575572043dfaa0dced2fd07aa3adf9d5
OriginalNVK <nguyenvankhanh27072004@gmail.com>
2025-01-13 10:27:52 +0700

    complete the feature add image into album

--- app/src/main/java/com/example/photo_gallery/utils/AlbumManager.java
+++ app/src/main/java/com/example/photo_gallery/utils/AlbumManager.java
@@ -1,6 +1,15 @@
 package com.example.photo_gallery.utils;
 
+import android.content.Context;
 import android.content.SharedPreferences;
+
+import com.example.photo_gallery.models.Album;
+import com.example.photo_gallery.models.ImageItem;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AlbumManager {
     private static final String PREF_NAME = "AlbumPrefs";
@@ -14,7 +23,7 @@ public class AlbumManager {
         gson = new Gson();
     }
 
-    public void saveAlbums(Lít<Album> albums)
+    public void saveAlbums(List<Album> albums)
     {
         String json = gson.toJson(albums);
         sharedPreferences.edit().putString(ALBUMS_KEY, json).apply();
@@ -65,14 +74,74 @@ public class AlbumManager {
     }
 
     public void addImageToAlbum(String albumName, String imagePath){
-        List<ALbum> albums = loadAlbums();
+        List<Album> albums = loadAlbums();
 
         for(Album album : albums)
         {
             if(album.getName().equals(albumName)){
-                album.addImage(new ImageItem(imagePath));
+               for(ImageItem image: album.getListImage()){
+                   if(image.getImagePath().equals(imagePath)){
+                       throw new IllegalArgumentException("Image already exists in the album");
+                   }
+               }
+
+               long dateToken = getDateToken(imagePath);
+               album.addImage(new ImageItem(imagePath, dateToken));
+               break;
+            }
+        }
+
+        saveAlbums(albums);
+    }
+
+    private long getDateToken(String imagePath){
+        Album allAlbum = getAlbumByName("All");
+
+        for(ImageItem image:  allAlbum.getListImage()){
+            if(image.getImagePath().equals(imagePath)){
+                return image.getDateToken();
+            }
+        }
+
+        return 0;
+    }
+
+    public void removeImageFromAlbum(String albumName, String imagePath){
+        List<Album> albums = loadAlbums();
+
+        for(Album album: albums){
+            if(album.getName().equals(albumName)){
+                for(ImageItem image: album.getListImage()){
+                    if(image.getImagePath().equals(imagePath)){
+                        album.removeImage(image);
+                        break;
+                    }
+                }
                 break;
             }
         }
+
+        saveAlbums(albums);
+    }
+
+    public List<String> getAlbumNames(String imagePath){
+        List<Album> albums = loadAlbums();
+        List<String> albumNames = new ArrayList<>();
+
+        for(Album album: albums)
+        {
+            if(album.getName().equals("ALl")){
+                continue;
+            }
+
+            for(ImageItem image: album.getListImage()){
+                if(image.getImagePath().equals(imagePath)){
+                    albumNames.add(album.getName());
+                    break;
+                }
+            }
+        }
+
+        return albumNames;
     }
 }
