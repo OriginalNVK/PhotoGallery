29856917832f4d735c438e02ac62cb30e3f555e1
OriginalNVK <nguyenvankhanh27072004@gmail.com>
2025-01-13 10:56:54 +0700

    Add feature group image follow date, month, year

--- app/src/main/java/com/example/photo_gallery/utils/ImageGrouping.java
+++ app/src/main/java/com/example/photo_gallery/utils/ImageGrouping.java
@@ -1,4 +1,42 @@
 package com.example.photo_gallery.utils;
 
+import com.example.photo_gallery.models.ImageItem;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class ImageGrouping {
+    public static Map<String, List<ImageItem>> groupByDate(List<ImageItem> imageList)
+    {
+        Map<String, List<ImageItem>> imageByDate = new HashMap<>();
+        for(ImageItem imageItem: imageList)
+        {
+            imageByDate.computeIfAbsent(imageItem.getDate(), k -> new ArrayList<>()).add(imageItem);
+        }
+        return imageByDate;
+    }
+
+    public static Map<String, List<ImageItem>> groupByMonth(List<ImageItem> imageList)
+    {
+        Map<String, List<ImageItem>> imageByMonth = new HashMap<>();
+        for(ImageItem imageItem: imageList)
+        {
+            imageByMonth.computeIfAbsent(imageItem.getMonth(), k -> new ArrayList<>()).add(imageItem);
+        }
+        return imageByMonth;
+
+    }
+
+    public static Map<String, List<ImageItem>> groupByYear(List<ImageItem> imageList)
+    {
+        Map<String, List<ImageItem>> imageByYear = new HashMap<>();
+        for(ImageItem imageItem: imageList)
+        {
+            imageByYear.computeIfAbsent(imageItem.getYear(), k -> new ArrayList<>()).add(imageItem);
+        }
+
+        return imageByYear;
+    }
 }
